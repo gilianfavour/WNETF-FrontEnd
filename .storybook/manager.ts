2a37fd2c63a4e1775bb38c8d348fd25dc16706fd
gilianfavour <gilianfavour06gmail.com>
2025-11-11 21:02:06 +0300

    folder structure

---
+++ .storybook/manager.ts
@@ -0,0 +1,13 @@
+import { addons } from '@storybook/manager-api';
+import { create } from '@storybook/theming';
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+
+    // Logo
+    brandTitle: 'TailNext',
+    brandUrl: '#',
+    brandTarget: '_blank',
+  }),
+});
